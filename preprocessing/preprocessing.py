b50728dd31e487b29608d326a1185f4e51c5bc68
Adrian M. Perez <95656917+AdrianPerez0306@users.noreply.github.com>
2024-11-25 18:12:34 -0300

    Update preprocessing.py

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,9 +1,9 @@
-import numpy as np
 import pandas as pd
 
 from cleaning_funcs import reemplazar_dni_nan, extract_columns_from_description, clean_materias, fill_empty_with_none, \
     clean_mascotas, clean_trabajo, clean_experiencia, clean_grupos, pipeline_inicial, trabajo, limpiar_no_trabajo
 
+path = '/home/the14th/Downloads/'
 df_alumnos_BdD = pd.read_csv('raw-data.csv')
 
 df_alumnos_BdD = pipeline_inicial(df_alumnos_BdD)
@@ -33,11 +33,11 @@ df_alumnos_BdD.rename(columns={"rol": "nombre_rol"}, inplace=True)
 
 # TABLA GRUPOS
 df_grupo = df_alumnos_BdD['nombre_grupo'].drop_duplicates()
-df_grupo.to_csv(r"/home/the14th/Downloads/grupos.csv", index=False)
+df_grupo.to_csv(f'{path}grupos.csv', index=False)
 
 # TABLA ROL
 df_rol = df_alumnos_BdD['nombre_rol'].drop_duplicates()
-df_rol.to_csv(r"/home/the14th/Downloads/roles.csv", index=False)
+df_rol.to_csv(f'{path}roles.csv', index=False)
 
 
 ###################################################################
@@ -47,7 +47,7 @@ df_rol.to_csv(r"/home/the14th/Downloads/roles.csv", index=False)
 df_materias = df_alumnos_BdD.explode('materias').reset_index(drop=True)
 df_materias.rename(columns={"materias": "nombre_materia"}, inplace=True)
 df_materias_unique = df_materias['nombre_materia'].drop_duplicates()
-df_materias_unique.to_csv(r"/home/the14th/Downloads/materias.csv", index=False)
+df_materias_unique.to_csv(f'{path}materias.csv', index=False)
 
 
 ###################################################################
@@ -65,7 +65,7 @@ df_profesiones_unique = pd.DataFrame({'nombre_profesion': df_trabajos})
 df_profesiones_unique = df_profesiones_unique[df_profesiones_unique['nombre_profesion'] != 'trabajo']
 df_profesiones_unique = df_profesiones_unique[df_profesiones_unique['nombre_profesion'] != 'no trabajo']
 df_profesiones_unique = df_profesiones_unique.dropna()
-df_profesiones_unique.to_csv(r"/home/the14th/Downloads/profesiones.csv", index=False)
+df_profesiones_unique.to_csv(f'{path}profesiones.csv', index=False)
 ###################################################################
 ###################################################################
 
@@ -77,7 +77,7 @@ df_profesiones_unique.to_csv(r"/home/the14th/Downloads/profesiones.csv", index=F
 localidades_unicas = df_alumnos_BdD['localidad'].explode().unique()
 df_localidades_unique = pd.DataFrame({'nombre_localidad': localidades_unicas})
 df_localidades_unique = df_localidades_unique[ 'nombre_localidad']
-df_localidades_unique.to_csv(r"/home/the14th/Downloads/localidades.csv", index=False)
+df_localidades_unique.to_csv(f'{path}localidades.csv', index=False)
 ###################################################################
 ###################################################################
 
@@ -90,7 +90,7 @@ df_localidades_unique.to_csv(r"/home/the14th/Downloads/localidades.csv", index=F
 df_hobbies = df_alumnos_BdD.explode('hobbies').reset_index(drop=True)
 df_hobbies.rename(columns={"hobbies": "nombre_hobbie"}, inplace=True)
 df_hobbies_unique = df_hobbies['nombre_hobbie'].drop_duplicates().dropna()
-df_hobbies_unique.to_csv(r"/home/the14th/Downloads/hobbies.csv", index=False)
+df_hobbies_unique.to_csv(f'{path}hobbies.csv', index=False)
 
 ###################################################################
 ###################################################################
@@ -104,7 +104,7 @@ df_hobbies_unique.to_csv(r"/home/the14th/Downloads/hobbies.csv", index=False)
 df_series = df_alumnos_BdD.explode('series').reset_index(drop=True)
 df_series.rename(columns={"series": "nombre_serie"}, inplace=True)
 df_series_unique = df_series['nombre_serie'].drop_duplicates().dropna()
-df_series_unique.to_csv(r"/home/the14th/Downloads/series.csv", index=False)
+df_series_unique.to_csv(f'{path}series.csv', index=False)
 
 # TABLA RECOMIENDA
 # df_series = df_series[['id_serie', 'nombre_serie', 'id_alumno']].dropna()
@@ -122,7 +122,7 @@ df_series_unique.to_csv(r"/home/the14th/Downloads/series.csv", index=False)
 df_musica = df_alumnos_BdD.explode('musica').reset_index(drop=True)
 df_musica.rename(columns={"musica": "nombre_musica"}, inplace=True)
 df_musica_unique = df_musica['nombre_musica'].drop_duplicates().dropna()
-df_musica_unique.to_csv(r"/home/the14th/Downloads/musica.csv", index=False)
+df_musica_unique.to_csv(f'{path}musica.csv', index=False)
 
 ###################################################################
 ###################################################################
@@ -143,6 +143,6 @@ df_alumnos_BdD['series'] = df_alumnos_BdD['series'].apply(lambda row: row if row
 df_alumnos_BdD['mascotas'] = df_alumnos_BdD['mascotas'].apply(lambda row: row if row is None else set(row))
 df_alumnos_BdD['localidad'] = df_alumnos_BdD['localidad'].apply(lambda row: row[0])
 df_alumnos_BdD['trabajo'] = df_alumnos_BdD['trabajo'].apply(lambda row: row if row is None else row[0])
-df_alumnos_BdD.to_csv(r"/home/the14th/Downloads/alumno.csv", index=False)
+df_alumnos_BdD.to_csv(f'{path}alumno.csv', index=False)
 ###################################################################
 ###################################################################
